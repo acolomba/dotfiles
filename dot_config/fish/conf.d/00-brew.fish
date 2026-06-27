7bd6a0b37e89d062ac69f111d3060a0683d709a0
Alessandro Colomba <acolomba@gmail.com>
2026-06-27 08:44:21 -0400

    reapply accidentally removed linux brew support

--- dot_config/fish/conf.d/00-brew.fish
+++ dot_config/fish/conf.d/00-brew.fish
@@ -1,5 +1,7 @@
 if test -x /opt/homebrew/bin/brew
     eval $(/opt/homebrew/bin/brew shellenv fish)
-
+    set -gx HOMEBREW_NO_ASK 1
+else if test -x /home/linuxbrew/.linuxbrew/bin/brew
+    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)
     set -gx HOMEBREW_NO_ASK 1
 end
